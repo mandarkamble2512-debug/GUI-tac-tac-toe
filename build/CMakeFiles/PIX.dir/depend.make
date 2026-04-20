4f8a386fd2eae6ee40c4c0b68b6e2fe9357fd1c3
Mandar <mandar@192.168.1.7>
2026-04-20 10:52:28 +0530

    Initial commit: basic project structure

---
+++ build/CMakeFiles/PIX.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PIX.
+# This may be replaced when dependencies are built.
---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+using std::atomic;
+using sf::RenderWindow;
+using sf::VideoMode;
+using sf::Event;
+using sf::Color;
+using sf::RectangleShape;
+using sf::Vector2f;
+using std::thread;
+using std::chrono::milliseconds;  
+using sf::Vector2u;
+using sf::CircleShape;
+
+atomic<bool> running(true);
+
+void workerThread() {
+    while (running) {
+        std::this_thread::sleep_for(milliseconds(100));
+    }
+}
+
+void DrawO (RenderWindow& window, float PosX, float PosY)
+{
+    int Radius = 100;
+    CircleShape OLetter(Radius);
+    OLetter.setFillColor(Color::White);
+    window.draw(OLetter);
+}
+
+void Drawbord(RenderWindow& window)
+{
+    Vector2u size = window.getSize();
+
+    RectangleShape bord(Vector2f(10, size.y - 20));
+    RectangleShape bord1(Vector2f(10, size.y - 20));
+    RectangleShape bord2(Vector2f(size.x -20,10));
+    RectangleShape bord3(Vector2f(size.x -20,10));
+
+    bord.setFillColor(Color::White);
+    bord.setPosition(size.x * 1/3, 10);
+    bord1.setFillColor(Color::White);
+    bord1.setPosition((size.x * 1/3) * 2, 10);
+    bord2.setFillColor(Color::White);
+    bord2.setPosition(10, size.y * 1/3);
+    bord3.setFillColor(Color::White);
+    bord3.setPosition(10,(size.y * 1/3) * 2);
+
+    window.draw(bord1);
+    window.draw(bord);
+    window.draw(bord2);
+    window.draw(bord3);
+}
+
+int main() {
+    RenderWindow window(VideoMode(800, 800), "PIX");
+
+    thread worker(workerThread);  
+
+    Event event;
+    while (running && window.isOpen()) {
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                running = false;
+            }
+        }
+
+        window.clear(Color::Black);
+        Drawbord(window);
+        // DrawO(window, 0, 0);
+        window.display();
+    }
+
+    running = false;
+    worker.join();
+    return 0;
+}
