efe2fdf588edb7ae308a3b4b9c323577bd96f75b
mandarkamble2512-debug <mandarkamble2512@gmail.com>
2026-05-23 19:42:57 +0530

    Day 19.4 Did some small changes to perevent future faliures

--- src/RenderObjects.hpp
+++ src/RenderObjects.hpp
@@ -1,10 +1,10 @@
+#pragma once;
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
 #include <atomic>
 #include <chrono>
 #include "logic.hpp"
-#pragma once;
 
 using std::ref;
 using std::endl;
